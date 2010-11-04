9917687d628383bb5efad37c1c3827dc4d36fdb7
Martin Smith <martinksmith@gmail.com>
2010-11-03 22:50:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalWhaleStudios::Application.config.secret_token = 'd974dd8654c76860af6bacb0527d068f9d845816ea7d08389f3beb887b38d5aefab3bb94bf73eb4a6c7b813b3ba39cff45b250d1148e7d561ec1f07a498c117d'
