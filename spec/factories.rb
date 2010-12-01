401d1afb5abfb686e0dcad74f0732783c22c7eaa
Martin Smith <martinksmith@gmail.com>
2010-11-30 22:37:24 -0600

    authentication and user pages added

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Martin Smith"
+  user.email                  "martinksmith@gmail.com"
+  user.password               "drywall"
+  user.password_confirmation  "drywall"
+end
