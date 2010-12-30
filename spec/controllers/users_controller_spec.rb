3adda0401a45a8f67d6806a3742a2e85863ea883
Martin Smith <martinksmith@gmail.com>
2010-12-29 22:08:26 -0600

    ripping out users and adding with devise

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,6 +34,16 @@ describe UsersController do
       get :new
       response.should have_selector("input[name='user[password_confirmation]'][type='password']")
     end
+    
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'new'
+      response.should have_selector("title", :content => "Sign up")
+    end
   end
   
   describe "GET 'show'" do
@@ -67,17 +77,28 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
   end
-
-  describe "GET 'new'" do
+  
+  describe "GET 'edit'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
     
     it "should be successful" do
-      get 'new'
+      get :edit, :id => @user
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
-      response.should have_selector("title", :content => "Sign up")
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+    
+    it "should have a link to change the Gravatar" do
+      get :edit, :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url, :content => "change")
     end
   end
   
